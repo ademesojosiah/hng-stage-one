6801595783df992102f9a006be11674055b3eee2
ademesojosiah <josiahademeso@gmail.com>
2023-09-11 08:50:53 +0100

    stage-one
    this commit sets up node js and typescript

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import http from "http";
+import url from "url";
+
+const server = http.createServer((req:any, res:any) => {
+	const parsedUrl = url.parse(req.url, true);
+
+	if (req.method === "GET" && parsedUrl.pathname === "/api") {
+		const { slack_name, track } = parsedUrl.query;
+		const date = new Date();
+
+		const days = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+		];
+
+		const resp = {
+			slack_name,
+			current_day: days[date.getDay()],
+			utc_time: date.toISOString().replace(/\.\d+/, ""),
+			track,
+			github_file_url:
+				"https://github.com/ademesojosiah/hng-stage-one/blob/main/src/index.ts",
+			github_repo_url: "https://github.com/ademesojosiah/hng-stage-one",
+			status_code: 200,
+		};
+
+		res.writeHead(200, { "Content-Type": "application/json" });
+		res.end(JSON.stringify(resp));
+	} else {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.end("Not Found\n");
+	}
+});
+
+server.listen(3001, () => {
+	console.log("server is up and running");
+});
